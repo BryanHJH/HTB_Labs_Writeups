d62bebab0ddaee9747a745ac2c0ec42a97bbbf7e
BryanHJH <2702bryan@gmail.com>
2025-06-12 16:51:41 +0800

    Complete LFI, started File-Uplod

---
+++ Academy/LFI/configure.php
@@ -0,0 +1,15 @@
+<?php
+
+if ($_SERVER['REQUEST_METHOD'] == 'GET' && realpath(__FILE__) == realpath($_SERVER['SCRIPT_FILENAME'])) {
+  header('HTTP/1.0 403 Forbidden', TRUE, 403);
+  die(header('location: /index.php'));
+}
+
+$config = array(
+  'DB_HOST' => 'db.inlanefreight.local',
+  'DB_USERNAME' => 'root',
+  'DB_PASSWORD' => 'HTB{n3v3r_$t0r3_pl4!nt3xt_cr3d$}',
+  'DB_DATABASE' => 'blogdb'
+);
+
+$API_KEY = "Awew242GDshrf46+35/k";
